ee98b7e5059197a35573fd7ac248a76a57c0289e
Scott McKeefer <Scottzapturk19@gmail.com>
2025-02-09 17:00:29 -0600

    Add title and basic consept for switching games

---
+++ panther-city-50/GlobalScripts/globals.gd
@@ -0,0 +1,3 @@
+extends Node
+
+#this file will store Global var that can be read from anywhere in the game
---
+++ panther-city-50/GlobalScripts/scenes.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# This global will keep track of Scene names for easy switching
+@onready var BaseGameTitle := "res://BaseGame/Scene/Title.tscn"
+@onready var GameSelection := "res://BaseGame/Scene/game_selection.tscn"
+
+
+
+# Panther Run Scenss
+@onready var PantherRunTitle := "res://PantherRun/PantherRunTitle.tscn"
+
+# Panther Escape Scenes 
+@onready var PantherEscapeTitle := "res://PanterEscapeFromZoo/PantherEscapeTitle.tscn"
