9547350a5c8b349223113a29075fe5e0a55be949
Scott McKeefer <Scottzapturk19@gmail.com>
2025-02-10 21:34:23 -0600

    Add add on and update title for ZE

--- panther-city-50/ZooEscape/Scripts/panther_escape_title.gd
+++ panther-city-50/ZooEscape/Scripts/panther_escape_title.gd
@@ -1,12 +1,31 @@
 extends Node2D
 
+@onready var newGamePos := Vector2(272, 244)
+@onready var passwordPos := Vector2(272, 272)
+@onready var selector = $Selector
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	selector.position = newGamePos
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene_to_file(Scenes.GameSelection)
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		if selector.position == newGamePos:
+			get_tree().change_scene_to_file(Scenes.ZEDebug)
+		elif selector.position == passwordPos:
+			pass # will make a password scean at some point
+			
+	if Input.is_action_just_pressed("ui_down") || Input.is_action_just_pressed("ui_up"):
+		swapSelectorPos()
+
+
+func swapSelectorPos() -> void:
+	if selector.position == newGamePos:
+		selector.position = passwordPos
+	else: 
+		selector.position = newGamePos
