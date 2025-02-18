69929bc8a4ce07417f342b94916ca65411dd081e
Scott McKeefer <Scottzapturk19@gmail.com>
2025-02-17 20:54:03 -0600

    Added Exit tile obj

--- panther-city-50/ZooEscape/Scripts/ZELevelManager.gd
+++ panther-city-50/ZooEscape/Scripts/ZELevelManager.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# this is the code to go to this level
+@export var LevelCode := "0000"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_exit_tile_player_exits(LevelToGoTo: PackedScene) -> void:
+	SceneManager.GoToNewScenePacked(self, LevelToGoTo)
+
+
+func _on_steak_manager_all_steak_collected() -> void:
+	$ExitTile.ActavateExit()
