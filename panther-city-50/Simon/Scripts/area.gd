991cd3e3895cb86d03126402813a43d6a0e3d08a
DevMichaelV <devmvogel@gmail.com>
2025-02-20 16:37:57 -0600

    Simon game updated
    Core gameplay loop implemented into the _process function and helper functions
    
    TODO soon:
    - Make the menu disappear while a game is running
    - Create HUD
    - Show score, lives, timer on HUD

--- panther-city-50/Simon/Scripts/area.gd
+++ panther-city-50/Simon/Scripts/area.gd
@@ -11,13 +11,11 @@ func _ready() -> void:
 	pass
 
 
+# Called when Input events are detected
 func _input(event: InputEvent) -> void:
-	if !is_area_locked:
-		if (event is InputEventMouseButton && event.pressed \
-		&& event.button_index == MOUSE_BUTTON_LEFT \
-		&& mouseEntered == true):
-			user_clicked_me.emit(name.split("_"))
-			print(name.get_slice("_", 1))
+	if mouseEntered:
+		if (event is InputEventMouseButton && event.pressed && event.button_index == MOUSE_BUTTON_LEFT):
+			user_clicked_me.emit(name.get_slice("_", 1))
 
 
 # Toggle the lock on this area
@@ -30,15 +28,16 @@ func toggle_area_lock():
 func update_area():
 	$Polygon2D.color = Color(color).darkened(dark_pct)
 
+
 # Mouse Entered Area
 func _on_mouse_entered() -> void:
+	mouseEntered = true
 	if !is_area_locked:
-		mouseEntered = true
 		$Polygon2D.color = Color(color)
 
 
 # Mouse Exited Area
 func _on_mouse_exited() -> void:
+	mouseEntered = false
 	if !is_area_locked:
-		mouseEntered = false
 		$Polygon2D.color = Color(color).darkened(dark_pct)
--- panther-city-50/Simon/Scripts/simon_title.gd
+++ panther-city-50/Simon/Scripts/simon_title.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called when input events happen
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
-		SceneManager.GoToNewSceneString(self, Scenes.GameSelection)
+		_on_quit_btn_button_up()
 		
 	if Input.is_action_just_pressed("ui_accept"):
 		SceneManager.GoToNewSceneString(self, Scenes.SimonTitle)
@@ -21,5 +21,10 @@ func _on_new_btn_button_up() -> void:
 
 
 # Quit Game Button UP
+ # Scene Tree checking for running this Scene as a standalone during development
+ # Can remove checking after development is complete
 func _on_quit_btn_button_up() -> void:
-	SceneManager.GoToNewSceneString(self, Scenes.GameSelection)
+	if get_parent().name == "GameRoot":
+		SceneManager.GoToNewSceneString(self, Scenes.GameSelection)
+	else:
+		get_tree().quit()
