992d68688931112df493cf3123295e31af72623b
Eric Burgess <ericdb@gmail.com>
2025-02-26 15:14:46 -0600

    drifter: add thrust and crash sounds

--- panther-city-50/Drifter/Scripts/ship.gd
+++ panther-city-50/Drifter/Scripts/ship.gd
@@ -12,6 +12,8 @@ const MAX_VELOCITY := 225.0	# pixels/sec
 @onready var exhaust_right: Polygon2D = $ExhaustRight
 @onready var collider := $Collider
 @onready var ship_shape := $ShipShape
+@onready var thrust_sound: AudioStreamPlayer = $ThrustSound
+@onready var crash_sound: AudioStreamPlayer = $CrashSound
 
 	
 func _ready() -> void:
@@ -29,7 +31,7 @@ func reset() -> void:
 func _on_area_entered(area: Area2D) -> void:
 	# Check if the collided area belongs to the group 'wall'
 	if area.is_in_group("walls"):
-		print("Ship collided with wall!")
+		crash_sound.play()
 		get_parent().ship_crashed()
 
 
@@ -42,6 +44,14 @@ func _process(_delta: float) -> void:
 		velocity += delta_v
 	velocity = clamp(velocity, -MAX_VELOCITY, MAX_VELOCITY)
 	position += Vector2(velocity * _delta, 0)
+	
+	# Play thrust sound while ship is thrusting
+	if thrusting_left or thrusting_right:
+		if not thrust_sound.playing:
+			thrust_sound.play()
+	else:
+		if thrust_sound.playing:
+			thrust_sound.stop()
 
 	
 func _input(_event: InputEvent) -> void:
